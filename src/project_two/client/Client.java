401c78efc1bcdfabd545829d9768fad95f9ad199
will-s-stone <will@williamstone.me>
2024-03-25 11:59:01 -0400

    Added in feature to send and receive some arbitrary file.

--- src/project_two/client/Client.java
+++ src/project_two/client/Client.java
@@ -1,12 +1,51 @@
 package project_two.client;
 
+import java.io.*;
+import java.net.Socket;
 import java.util.concurrent.TimeUnit;
 
 public class Client {
-    public static void main(String[] args) throws InterruptedException {
-        while(true){
-            System.out.println("hey");
-            TimeUnit.SECONDS.sleep(2);
+    String host;
+    int port;
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Client client = new Client("localhost", 1234);
+        client.sendFile("C:/Users/stone/main_dir/suny_oswego/spring_24/csc_445/code/CSC-445/src/project_two/additional/practice_file.txt", 1024);
+
+    }
+    public Client(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+
+    public void sendFile(String filePath, int windowSize) throws IOException {
+        try (Socket socket = new Socket(host, port);
+             FileInputStream fileInputStream = new FileInputStream(filePath);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
+
+            // Send file name and length to the server
+            File file = new File(filePath);
+            dataOutputStream.writeUTF(file.getName());
+            dataOutputStream.writeLong(file.length());
+
+            // Send file contents to the server in chunks of 1024 bytes
+            // When reading from the file, the read(buffer) method will read up to 1024 bytes
+            // from the file into the buffer. If the file has fewer than 1024 bytes remaining,
+            // it will read the remaining bytes and return the actual number of bytes read.
+            // This way, the entire file can be sent in multiple chunks, each up to 1024 bytes in size,
+            // without any limitation on the total size of the file.
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                dataOutputStream.write(buffer, 0, bytesRead);
+            }
+            dataOutputStream.flush();
+            System.out.println("File sent successfully.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+
 }
--- src/project_two/server/Server.java
+++ src/project_two/server/Server.java
@@ -3,37 +3,53 @@ package project_two.server;
 import project_two.additional.Packet;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.TimeUnit;
 
 public class Server {
-    public static void main(String[] args) throws InterruptedException {
+    int port;
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Server server = new Server(1234);
+        server.receiveFile("I'll add this later", 69);
+
 
     }
 
-    public void sendFile(String host, int port, String filepath, int windowSize) throws IOException {
-        Socket socket = new Socket(host, port);
-        ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+    public Server(int port) {
+        this.port = port;
+    }
+    public void receiveFile(String filepath, int windowSize) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket socket = serverSocket.accept();
+             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream())) {
 
-        File file = new File(filepath);
-        FileInputStream fileInputStream = new FileInputStream(file);
-        byte[] fileData = new byte[(int) file.length()];
-        fileInputStream.read(fileData);
+            // Receive file name and length from the client
+            String fileName = dataInputStream.readUTF();
+            long fileLength = dataInputStream.readLong();
 
-        int startPos = 0;
-        int nextSeq = 0;
+            // Create a new file on the server
+            File file = new File("received_" + fileName);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+                 BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
 
-        while (startPos < fileData.length){
-            //Cha Cha Slide here
-            while(nextSeq < startPos + windowSize && nextSeq < fileData.length){
-                Packet packet = new Packet(nextSeq, fileData[nextSeq]);
+                // Receive file contents from the client and write to the file
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while (fileLength > 0 && (bytesRead = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, fileLength))) != -1) {
+                    bufferedOutputStream.write(buffer, 0, bytesRead);
+                    fileLength -= bytesRead;
+                }
+                bufferedOutputStream.flush();
+                System.out.println("File received successfully.");
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
     }
-
 
 }
