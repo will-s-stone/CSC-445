0c1f327f1effc936ad23d91589ebd7b2d7622b49
will-s-stone <will@williamstone.me>
2024-04-04 13:15:49 -0400

    Working on selectors in separate directory...

--- src/project_two/client/Client.java
+++ src/project_two/client/Client.java
@@ -2,173 +2,65 @@ package project_two.client;
 
 import project_two.additional.TFTP;
 
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 public class Client extends TFTP {
-    String host;
-    int port;
-    TreeMap<Short, byte[]> data = new TreeMap<>();
+    private String HOST;
+    private int PORT;
+    TreeMap<Short, byte[]> packets = new TreeMap<>();
 
     //Could have the DatagramChannel as an instance var up here.
-    // DatagramChannel channel;
+    DatagramChannel CHANNEL;
+    InetSocketAddress ADDRESS;
     // Selector selector;
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        Client client = new Client("localhost", 1234);
-        client.sendAndReceiveAck("C:/Users/stone/main_dir/suny_oswego/spring_24/csc_445/code/CSC-445/src/project_two/additional/practice_file.txt", 8);
+        Client client = new Client("localhost", 12345);
+        client.start("C:/Users/stone/main_dir/suny_oswego/spring_24/csc_445/code/CSC-445/src/project_two/additional/practice_file.txt", 8);
         //client.loadFile("C:/Users/stone/main_dir/suny_oswego/spring_24/csc_445/code/CSC-445/src/project_two/additional/practice_file.txt");
     }
-    public Client(String host, int port){
-        this.host = host;
-        this.port = port;
+    public Client(String host, int port) throws IOException {
+        CHANNEL = DatagramChannel.open();
+        CHANNEL.configureBlocking(false);
+        this.HOST = host;
+        this.PORT = port;
+        ADDRESS = new InetSocketAddress(HOST, PORT);
     }
 
-    public void sendFile(String filePath, int windowSize) throws IOException {
-        int blockNumLastAckReceived = 0; //seqNum in book
-        int blockNumLastFrameSent = 0;
-        List<Short> ackedBlocks = new ArrayList<>();
-        Selector selector = Selector.open();
-        // Selector resource: https://www.baeldung.com/java-nio-selector
-        DatagramChannel channel = DatagramChannel.open();
-        //channel.configureBlocking(true);
-        channel.connect(new InetSocketAddress(host, port));
-        channel.configureBlocking(false);
-
-        channel.register(selector, SelectionKey.OP_READ);
-
-
+    public void start(String filePath, int windowSize) throws IOException {
+        //load the file and write the data map
         loadFile(filePath);
-        int i = 0;
-
-//        Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-//        while (iterator.hasNext()) {
-//            SelectionKey key = iterator.next();
-//            iterator.remove();
-//            if (key.isReadable()) {
-//
-//                ByteBuffer buffer = ByteBuffer.allocate(2);
-//                channel.receive(buffer);
-//                buffer.flip();
-//
-//                short blockNum = buffer.getShort();
-//                ackedBlocks.add(blockNum);
-//
-//                //ackedBlocks.get(buffer.getShort());
-//                System.out.println("Received Ack " + buffer.getShort());
-//            }
-//            key.cancel();
-//        }
-
-        for (byte[] packetData : data.values()) {
-            // if(i < data.values().size()) won't be needed if I keep track of SWS, LAR, and LFS
-            // Put Iterator block here?
-
-            //  \/ Pointless for now but will come in handy with the window size
-            if (i < data.values().size()) {
-                ByteBuffer packet = ByteBuffer.wrap(packetData);
-                channel.write(packet);
-                System.out.println("Packet sent");
-            }
-
-
-            //while(true){
-                selector.selectNow(); // Non-blocking select?
-                Set<SelectionKey> keys = selector.selectedKeys();
-                Iterator<SelectionKey> keyIterator = keys.iterator();
-
-                while(keyIterator.hasNext()){
-                    SelectionKey key = keyIterator.next();
-                    if (key.isReadable()){
-                        System.out.println("STUFF IS HERE!!!");
-                        //keyIterator.remove();
-                    }
-                    keyIterator.remove();
-                }
-            //}
-//            ByteBuffer ackBuffer = ByteBuffer.allocate(2);
-//            channel.read(ackBuffer);
-//
-//            if (ackBuffer.position() > 0){
-//                // Flip from writing to reading :)
-//                ackBuffer.flip();
-//                short ack = ackBuffer.getShort();
-//                System.out.println("Received Ack on Client: " + ack);
-//            } else {
-//                System.out.println("Nada this go-round");
-//            }
-//            ackBuffer.clear();
-            //i++;
-        }
-        //}
-        //System.out.println("File sent successfully.");
-    }
-
-    //public void recieveAcks()
-
-    public void sendAndReceiveAck(String filePath, int windowSize) throws IOException {
-        loadFile(filePath);
-        int bufferSize = 1024;
-        Selector selector = Selector.open();
-        DatagramChannel channel = DatagramChannel.open();
-        channel.configureBlocking(false);
-        channel.connect(new InetSocketAddress(host, port));
-        channel.register(selector, SelectionKey.OP_CONNECT | SelectionKey.OP_READ, SelectionKey.OP_WRITE);
-
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-
-        while(true) {
-            selector.select();
-
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-
-            while(keyIterator.hasNext()){
-                SelectionKey key = keyIterator.next();
-                keyIterator.remove();
-
-                if (!key.isValid()){
-                    continue;
-                }
-                if (key.isConnectable()){
-                    System.out.println("Connected to server.");
-                }
-                // Read what is coming back to us
-                if (key.isReadable()){
-                    buffer.clear();
-                    short bytesRead = (short) channel.read(buffer);
-                    if (bytesRead == -1){
-                        channel.close();
-                        break;
-                    }
-                    buffer.flip();
-                    System.out.println("Received: " + new String(buffer.array(), 0, bytesRead));
-                }
-
-                if(key.isWritable()){
-                    //Make dynamic with data
-                    for (short i = 0; i < windowSize; i++) {
-                        byte[] packetData = data.get(i);
-                        buffer.clear();
-                        buffer.put(packetData);
-                        buffer.flip();
-                        channel.write(buffer);
-                        System.out.println("Packet number sent => " + i);
-                    }
-
-                }
+        while(true){
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            for (short i = 0; i < packets.size(); i++) {
+                sendFrame(i, buffer);
+                receiveMessage();
             }
         }
+
     }
 
+    public void receiveMessage() throws IOException {
+        ByteBuffer ackBuffer = ByteBuffer.allocate(2);
+        InetSocketAddress serverAddress = (InetSocketAddress) CHANNEL.receive(ackBuffer);
+        if (serverAddress != null) {
+            ackBuffer.flip();
+            short ack = ackBuffer.getShort();
+            System.out.println("Received ack from: " + ack);
+        } else {System.out.println("Nothing quite yet"); }
+    }
+
+    public void sendFrame(short blockNum, ByteBuffer buffer) throws IOException {
+        buffer.clear();
+        buffer.put(packets.get(blockNum));
+        //buffer = ByteBuffer.wrap(data.get(blockNum));
+        CHANNEL.send(buffer, ADDRESS);
+        System.out.println("Frame #" + blockNum + " ...");
+    }
 
 
 
@@ -183,7 +75,7 @@ public class Client extends TFTP {
             int bytesRead;
 
             while ((bytesRead = fis.read(buffer, BLOCK_NUM_SIZE, BLOCK_SIZE)) != -1) {
-                blockNum++;
+
                 //buffer[0] = (byte) ((blockNum >> 8) & 0xFF);
                 //buffer[1] = (byte) (blockNum & 0xFF);
                 ByteBuffer blockNumBuffer = ByteBuffer.allocate(BLOCK_NUM_SIZE);
@@ -195,9 +87,10 @@ public class Client extends TFTP {
                 // Copies the blockNum over to the dataBlock[]
                 dataBlock[0] = bytes[0];
                 dataBlock[1] = bytes[1];
-                data.put(blockNum, dataBlock);
+                packets.put(blockNum, dataBlock);
                 //ByteBuffer packet = ByteBuffer.wrap(buffer, 0, bytesRead + BLOCK_NUM_SIZE);
                 //data.put(blockNum, packet.array().clone());
+                blockNum++;
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/project_two/server/Server.java
+++ src/project_two/server/Server.java
@@ -9,10 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeMap;
@@ -20,106 +17,45 @@ import java.util.concurrent.TimeUnit;
 
 public class Server extends TFTP {
     int port;
+    TreeMap<Short, byte[]> packets = new TreeMap<>();
+    private static final int PORT = 12345;
+    private static final int BUFFER_SIZE = 1024;
+    private InetSocketAddress ADDRESS;
+    private DatagramChannel CHANNEL;
+
+    public Server() throws IOException {
+        CHANNEL = DatagramChannel.open();
+        CHANNEL.socket().bind(new InetSocketAddress(PORT));
+        CHANNEL.configureBlocking(false);
+        System.out.println("Server started on port " + PORT);
+    }
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        Server server = new Server(1234);
-        server.receiveFileAndSendAck("testing_file_123.txt", 8);
+        Server server = new Server();
+        server.start();
     }
 
-    public Server(int port) {
-        this.port = port;
-    }
+    public void start() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        while(true){
+            buffer.clear();
+            InetSocketAddress clientAddress = (InetSocketAddress) CHANNEL.receive(buffer);
+            if(clientAddress != null){
+                buffer.flip();
+                String message = new String(buffer.array(), 0, buffer.limit());
+                System.out.println("Received message from " + clientAddress + ": " + message);
+                String response = "Server response to " + clientAddress;
+                buffer.clear();
+                buffer.put(response.getBytes());
+                buffer.flip();
 
-    public void receiveFile(String filepath, int windowSize) throws IOException {
-        DatagramChannel channel = DatagramChannel.open();
-        channel.bind(new InetSocketAddress(port));
-
-        TreeMap<Short, byte[]> packetBuffer = new TreeMap<>();
-
-        while (true) {
-            //Make this a variable that is sent at first when a window size is requested.
-            ByteBuffer buffer = ByteBuffer.allocate(BLOCK_SIZE + BLOCK_NUM_SIZE);
-            SocketAddress clientAddress = channel.receive(buffer);
-            buffer.flip();
-            short blockNum = (short) (((buffer.get() & 0xff) << 8) | (buffer.get() & 0xff));
-            System.out.println("Block: " + blockNum);
-            boolean isLastPacket = buffer.remaining() < (BLOCK_SIZE + BLOCK_NUM_SIZE);
-
-            byte[] data = new byte[buffer.remaining()];
-            buffer.get(data);
-            packetBuffer.put(blockNum, data);
-
-            File file = new File(filepath);
-            try (FileOutputStream fos = new FileOutputStream(file, true)) {
-                // This loop seems problematic
-                while (packetBuffer.containsKey(blockNum)) {
-                    byte[] packetData = packetBuffer.remove(blockNum);
-                    fos.write(packetData);
-
-                    // Send the ack back, in this case it's just the block number/sequence number of the last frame received
-                    ByteBuffer ack = ByteBuffer.allocate(2);
-                    ack.putShort(blockNum);
-                    ack.flip();
-
-                    ack.position(0);
-                    channel.send(ack, clientAddress);
-                    System.out.println("\n\n Ack sent => " + blockNum);
-
-                    //if (isLastPacket) {fos.close();}
-                    blockNum++;
-                }
-                if (isLastPacket) {
-                    fos.close();
-                }
+                byte[] ack = {0, 7};
+                ByteBuffer ackBuffer = ByteBuffer.wrap(ack);
+                //CHANNEL.send(ackBuffer, clientAddress);
             }
-
-            String message = new String(data);
-            System.out.println("Received message from " + clientAddress + ": " + message);
         }
+
+
     }
 
-    public void receiveFileAndSendAck(String filepath, int windowSize) throws IOException {
-        try {
-            Selector selector = Selector.open();
-            DatagramChannel channel = DatagramChannel.open();
-            channel.configureBlocking(false);
-            channel.bind(new InetSocketAddress(port));
-            channel.register(selector, SelectionKey.OP_ACCEPT | SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-
-            ByteBuffer buffer = ByteBuffer.allocate(1024);
-
-            while (true) {
-                selector.select();
-
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-
-                while (keyIterator.hasNext()) {
-                    SelectionKey key = keyIterator.next();
-                    keyIterator.remove();
-
-                    if (!key.isValid()) {
-                        continue;
-                    }
-                    if (key.isAcceptable()) {
-                        System.out.println("This may or may not be connection from a client: " + channel.getRemoteAddress());
-                    }
-                    if (key.isReadable()) {
-                        DatagramChannel clientChannel = (DatagramChannel) key.channel();
-                        buffer.clear();
-                        InetSocketAddress clientAddress = (InetSocketAddress) clientChannel.receive(buffer);
-                        buffer.flip();
-
-                        byte[] packet = buffer.array();
-                        System.out.println(packet.toString());
-
-                        buffer.flip();
-                        clientChannel.send(buffer, clientAddress);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
